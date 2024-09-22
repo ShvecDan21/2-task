ae6217bb43f494d5328044e9896f7df21822c5dd
ShvecDan21 <shvecovd16@gmail.com>
2024-09-22 15:43:31 +0300

    Add files via upload

---
+++ 2task.py
@@ -0,0 +1,5 @@
+number_task = 12
+number_hours = 1.5
+name_course = 'Python'
+hour_one_task = number_hours/number_task
+print('Курс:', name_course + ', всего задач:' + str(number_task) + ', затрачено часов: '+ str(number_hours) + ', среднее время выполнения' ,(hour_one_task), 'часов')
